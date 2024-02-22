b6d4ef3d7d9b0feb83eeb710203bb70dabc93d43
harusee17 <117976995+harusee17@users.noreply.github.com>
2024-02-22 18:03:45 +0800

    Changed the maze
    Added panels, expanded the maze, improved the styles

--- pathfinder/src/index.ts
+++ pathfinder/src/index.ts
@@ -1,7 +1,7 @@
 import { maze, paintCells } from "./grid";
 
-const walls = maze.flat().filter(({wall})=> wall)
-const paths = maze.flat().filter(({wall})=> !wall)
+const walls = maze.flat().filter(({ wall }) => wall);
+const paths = maze.flat().filter(({ wall }) => !wall);
 
-paintCells(walls, "#f00")
-paintCells(paths, "#fff")
+paintCells(walls, "#000");
+paintCells(paths, "#fff");
